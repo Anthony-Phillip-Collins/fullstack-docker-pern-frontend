357fc2bf95f6c98d824c4c2b3d61e87703c0bfef
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-06-28 14:12:42 +1000

    removed logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import BlogContainer from './components/Blogs/BlogContainer';
-import blogService from './services/blog.service';
 
 function App() {
-  console.log(import.meta.env);
-
-  const f = async () => {
-    const blogs = await blogService.getAll();
-    console.log(blogs);
-  };
-
-  f();
-
   return <BlogContainer />;
 }
 
