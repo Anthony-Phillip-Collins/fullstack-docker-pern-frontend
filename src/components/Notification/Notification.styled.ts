024d6b96d2ce1c1056cca115fea57cb2f64c753f
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-08-04 21:51:34 +1000

    notification slide in

--- src/components/Notification/Notification.styled.ts
+++ src/components/Notification/Notification.styled.ts
@@ -1,25 +1,30 @@
-import { styled } from 'styled-components';
+import { keyframes, styled } from 'styled-components';
 import { FontWeight } from '../../styles/types';
 import { NotificationInnerProps } from './Notification';
+
+const slideIn = keyframes`
+  from { transform: translate(100%); }
+  to { transform: translate(0%); }
+`;
 
 export const StyledNotification = styled.div`
   position: absolute;
   right: 0;
   bottom: 0;
   overflow: hidden;
-  display: flex;
-  justify-content: flex-end;
 `;
 
 export const NotificationInner = styled.div.withConfig({
-  shouldForwardProp: (prop) => !['error', 'show', 'offsetX'].includes(prop),
+  shouldForwardProp: (prop) => !['error'].includes(prop),
 })<NotificationInnerProps>`
   display: inline-flex;
   align-items: center;
   justify-content: center;
+  animation: ${slideIn} 0.4s ease-out forwards;
 
-  ${({ theme, error, offsetX }) => {
-    const errorStyles = error
+  ${({ theme, error }) => {
+    const borderRadius = theme.global.borderRadius;
+    const colors = error
       ? {
           color: theme.colors.textOnDanger,
           backgroundColor: theme.colors.danger,
@@ -28,18 +33,18 @@ export const NotificationInner = styled.div.withConfig({
           color: theme.colors.textOnSuccess,
           backgroundColor: theme.colors.success,
         };
-    const borderRadius = theme.global.borderRadius;
+
     return {
-      transform: `translateX(${offsetX}px)`,
       fontSize: theme.fonts.body.fontSize,
       fontWeight: FontWeight.medium,
       borderRadius: `${borderRadius} 0 0 ${borderRadius}`,
       padding: theme.spacing.xl,
       marginBottom: theme.spacing.xl,
-      ...errorStyles,
+      ...colors,
     };
-  }}
+  }};
 `;
+
 export const Message = styled.span`
   margin-right: ${({ theme }) => theme.spacing.md};
 `;
--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,4 +1,3 @@
-import { useLayoutEffect, useRef, useState } from 'react';
 import { clearNotification } from '../../app/features/notification.slice';
 import { useAppDispatch } from '../../app/hooks';
 import { NotificationType } from '../../types/notification.type';
@@ -8,8 +7,6 @@ import { Message, NotificationInner, StyledNotification } from './Notification.s
 
 export interface NotificationInnerProps {
   error: boolean;
-  show?: boolean;
-  offsetX?: number;
 }
 
 type Props = NotificationType;
@@ -18,24 +15,11 @@ function Notification({ message, error }: Props) {
   const dispatch = useAppDispatch();
   const err = parseError(error);
   const msg = err?.message || message;
-  const innerRef = useRef<HTMLDivElement>(null);
-  const [offsetX, setOffsetX] = useState<number>(0);
-
-  // need to fix this
-
-  useLayoutEffect(() => {
-    if (innerRef.current) {
-      // setOffsetX(innerRef.current.offsetWidth * 0.5);
-      setOffsetX(0);
-      console.log(offsetX);
-    }
-  }, [offsetX]);
-
   return (
     <>
       {msg && (
         <StyledNotification>
-          <NotificationInner error={!!error} show={!!msg} ref={innerRef} offsetX={offsetX}>
+          <NotificationInner error={!!error}>
             <Message>{msg}</Message>
             <IconButton
               iconProps={{ icon: 'close' }}
