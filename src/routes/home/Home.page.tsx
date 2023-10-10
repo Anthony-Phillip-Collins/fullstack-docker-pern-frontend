671a864d4c1d1c322133d7a64d0eb5a3702749b4
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-10-10 10:36:20 +1000

    minor text amends

--- src/routes/home/Home.page.tsx
+++ src/routes/home/Home.page.tsx
@@ -9,15 +9,11 @@ const HomePage = () => {
       <h1>Welcome,</h1>
       <p>
         My name is Anthony Collins and this is my final submission for the{' '}
-        <ExternalLink href="https://fullstackopen.com/en/about/">Full stack course</ExternalLink> held at the Department
+        <ExternalLink href="https://fullstackopen.com/en/about/">full stack course</ExternalLink> held at the Department
         of Computer Science at the University of Helsinki. My aim for this project was to consolidate everything I have
-        learnt from each one of the 14 parts of the course. If you would like to take a look at the source code or run
-        the project locally then feel free to{' '}
-        <ExternalLink href="https://github.com/Anthony-Phillip-Collins/fullstack-docker-pern">
-          clone the project and its submodules
-        </ExternalLink>
-        {'. '}
-        If you would like to find out more about me please visit{' '}
+        learned from each one of the 14 parts of the course. Feel free to check out the{' '}
+        <ExternalLink href="https://github.com/Anthony-Phillip-Collins/fullstack-docker-pern">source code</ExternalLink>{' '}
+        and run the project locally. If you would like to find out more about me please visit{' '}
         <ExternalLink href="https://anthonycollins.net">https://anthonycollins.net</ExternalLink>.
       </p>
 
